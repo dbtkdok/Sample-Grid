609119ea95c7b7366373009ba0c5a396534e849c
dbtkdok <57377065+dbtkdok@users.noreply.github.com>
2022-03-10 09:08:49 +0900

    Add files via upload

---
+++ js/posMain.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  var trigger = $(".hamburger"),
+    overlay = $(".overlay"),
+    isClosed = false;
+
+  trigger.click(function () {
+    hamburger_cross();
+  });
+
+  function hamburger_cross() {
+    if (isClosed == true) {
+      overlay.hide();
+      trigger.removeClass("is-open");
+      trigger.addClass("is-closed");
+      isClosed = false;
+    } else {
+      overlay.show();
+      trigger.removeClass("is-closed");
+      trigger.addClass("is-open");
+      isClosed = true;
+    }
+  }
+
+  $('[data-toggle="offcanvas"]').click(function () {
+    $("#wrapper").toggleClass("toggled");
+  });
+
+  
+});
+$(function(){
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+
+    var href = $(e.target).attr('href');
+    var $curr = $(".process-model  a[href='" + href + "']").parent();
+
+    $('.process-model li').removeClass();
+
+    $curr.addClass("active");
+    $curr.prevAll().addClass("visited");
+  });
+});
